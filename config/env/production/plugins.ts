838f854e7dabf6545da835593add4c261b69ff6d
bolg55 <kellen.bolger@strapi.io>
2025-03-12 10:54:20 -0400

    fix: update config

--- config/env/production/plugins.ts
+++ config/env/production/plugins.ts
@@ -1,29 +1,27 @@
 export default ({ env }) => ({
-    // ...
-    upload: {
-        config: {
-            provider: 'aws-s3',
-            providerOptions: {
-                baseUrl: env('CDN_URL'),
-                forcePathStyle: false,
-                rootPath: env('CDN_ROOT_PATH'),
-                s3Options: {
-                    accessKeyId: env('AWS_ACCESS_KEY_ID'),
-                    secretAccessKey: env('AWS_ACCESS_SECRET'),
-                    region: env('AWS_REGION'),
-                    params: {
-                        ACL: env('AWS_ACL', 'public-read'),
-                        signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
-                        Bucket: env('AWS_BUCKET'),
-                    },
-                },
-            },
-            actionOptions: {
-                upload: {},
-                uploadStream: {},
-                delete: {},
-            },
+  // ...
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        s3Options: {
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          region: env('AWS_REGION'),
+          params: {
+            ACL: env('AWS_ACL', 'public-read'),
+            signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+            Bucket: env('AWS_BUCKET'),
+          },
+          s3ForcePathStyle: true,
         },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
     },
-    // ...
-});
+  },
+  // ...
+})
