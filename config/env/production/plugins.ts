dce64a68ed034035cbd1b02afffcde3855ea726a
bolg55 <kellen.bolger@strapi.io>
2025-03-18 12:30:33 -0400

    fix: test remove signedUrlExpires

--- config/env/production/plugins.ts
+++ config/env/production/plugins.ts
@@ -10,7 +10,7 @@ export default ({ env }) => ({
           region: env('AWS_REGION'),
           params: {
             ACL: env('AWS_ACL', 'public-read'),
-            signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+            // signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
             Bucket: env('AWS_BUCKET'),
           },
         },
